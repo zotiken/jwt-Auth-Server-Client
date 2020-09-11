6970acafbc134fc8213439e3c1222c4c5df7ef98
Лелявин Никита <n.lelyavin@corp.zonatelecom.ru>
2020-09-11 11:32:19 +0300

    added support deploy

--- appTest.js
+++ appTest.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const path = require('path');
+const path = require('path');
 // const cookieParser = require('cookie-parser');
 // const logger = require('morgan');
 // const mongoose = require('mongoose');
