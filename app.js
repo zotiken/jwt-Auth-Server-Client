848615ccb37072527eb79db70f2ddfea13a14168
Лелявин Никита <n.lelyavin@corp.zonatelecom.ru>
2020-09-11 15:33:33 +0300

    added support deployhm

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 const express = require('express');
-// const path = require('path');
-// const cookieParser = require('cookie-parser');
-// const logger = require('morgan');
-// const mongoose = require('mongoose');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 
-// const authRouter = require('./routes/auth');
-// const usersRouter = require('./routes/users');
-// const { send } = require('process');
+const authRouter = require('./routes/auth');
+const usersRouter = require('./routes/users');
+const { send } = require('process');
 
 const app = express();
 
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.use('/app/api/auth', authRouter);
-// app.use('/users', usersRouter);
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/app/api/auth', authRouter);
+app.use('/users', usersRouter);
 app.get("/",(req,res)=>{
-    res.send("First")
+    res.sendFile(__dirname, "client", "build", "index.html")
 })
 
 module.exports = app;
