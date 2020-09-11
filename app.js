a3251d488e199d8c57809e3734927b09b3a709d9
Лелявин Никита <n.lelyavin@corp.zonatelecom.ru>
2020-09-11 16:22:44 +0300

    added support deployhm

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname,"client", 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 app.use('/app/api/auth', authRouter);
 app.use('/users', usersRouter);
 app.get("/",(req,res)=>{
