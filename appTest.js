37f182473fee1b0a6f52442bbbf7308ecd612ba1
Лелявин Никита <n.lelyavin@corp.zonatelecom.ru>
2020-09-11 11:31:21 +0300

    added support deploy

--- appTest.js
+++ appTest.js
@@ -1,27 +1,27 @@
 const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const mongoose = require('mongoose');
+// const path = require('path');
+// const cookieParser = require('cookie-parser');
+// const logger = require('morgan');
+// const mongoose = require('mongoose');
 
-const authRouter = require('./routes/auth');
-const usersRouter = require('./routes/users');
-const { send } = require('process');
+// const authRouter = require('./routes/auth');
+// const usersRouter = require('./routes/users');
+// const { send } = require('process');
 
 const app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/app/api/auth', authRouter);
-app.use('/users', usersRouter);
+// app.use('/app/api/auth', authRouter);
+// app.use('/users', usersRouter);
 app.get("*",(req,res)=>{
     res.send("First")
 })
 
-app.listen("/",()=>{
+app.listen(()=>{
     console.log('!dfds')
 })
 
